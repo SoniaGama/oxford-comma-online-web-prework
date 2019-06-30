f8b8887c9166c7f3fd315afce02cac414fe603a0
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-06-30 22:18:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   if array.size < 3
     array.join(" and ")
   else 
-    array.last = "and #{array.last}"
+    array[-1] = "and #{array[-1]}"
     array.join(", ")
   end
     
