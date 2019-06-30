a5bec052b054abea023b3648860b9262279a3516
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-06-30 22:19:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,8 @@
 def oxford_comma(array)
-  #array.join(" and ")
-  #array.last = "and #{array.last}"
   if array.size < 3
     array.join(" and ")
   else 
     array[-1] = "and #{array[-1]}"
     array.join(", ")
   end
-  # "kiwi, durian, starfruit, mangos, dragon fruits, lychees, and pomelos"
 end
