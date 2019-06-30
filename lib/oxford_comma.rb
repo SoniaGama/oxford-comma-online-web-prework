ea4d8bac1d1513f07dc0c132aa211523ecda2705
Sonia Gama de la Cruz <abinyuuu@gmail.com>
2019-06-30 22:17:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  #array.join(" and ")
+  #array.last = "and #{array.last}"
+  if array.size < 3
+    array.join(" and ")
+  else 
+    array.last = "and #{array.last}"
+    array.join(", ")
+  end
+    
+  # "kiwi, durian, starfruit, mangos, dragon fruits, lychees, and pomelos"
+  #iterar y contar si nos 3 o mas agregar and al final 
+  #"pomelos" -> "pomelos"
 end
